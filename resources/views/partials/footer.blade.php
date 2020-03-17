ec0037c4eb217e1ecf9cf1efd048d26f953f24a8
rafaliq <rafal@creativeroom.pl>
2020-03-17 15:58:22 +0100

    contact

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,31 @@
-<section class="section">
-  <div class="container">
-    @if(get_field('show_map'))
-      {!! do_shortcode('[google_map_easy id="1"]') !!}
-    @endif
-  </div>
-</section>
+@if(get_field('show_map'))
+  <section class="section">
+    <div class="container contact">
+      <div class="contact__content">
+        <h2 class="title">
+          Kontakt
+        </h2>
+        <div class="text contact__text">
+          <p>
+            {{  get_option_field("name")  }}
+          </p>
+          <p>
+            {!! get_option_field("address") !!}
+          </p>
+          <p>
+            Telefon: 
+            <a href="tel:{{ str_replace(' ','', get_option_field('phone')) }}" class="link">
+              {{ get_option_field("phone") }}
+            </a>
+          </p>
+        </div>
+      </div>
+      <div class="contact__map">
+        {!! do_shortcode('[google_map_easy id="1"]') !!}
+      </div>
+    </div>
+  </section>
+@endif
 <footer class="footer">
   <div class="container">
     <div class="footer__row">
