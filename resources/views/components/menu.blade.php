fceb50e345ec3f858a01f10006ad24fd0598e800
rafaliq <rafal@creativeroom.pl>
2020-04-21 11:36:24 +0200

    pizza menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -18,50 +18,65 @@
         <div class="menu__bg">
           <img class="menu__img" src="@asset('./images/pizzabg.jpg')" alt="pizza">
         </div>
-        <div class="container menu__content menu__content--active" menu-content="pizza">
-          <div class="menu__columns">
-            <table class="table">
-              @foreach(get_option_field("menu")[0]['content']['list'] as $item)
-                @php
-                  $half = $loop->count/2
-                @endphp
-                @if($loop->iteration <= $half)
-                  <tr>
-                    <td>
+        @foreach(get_option_field("menu") as $item)
+          @php
+            $content = $item['content']
+          @endphp
+          <div class="container menu__content @if($loop->first) menu__content--active @endif" menu-content="{{ $content['title'] }}">
+            @php
+              $countrows = count($content['list'])/2;
+            @endphp
+            @if($content['menu-type'] == 'pizza')
+              <div class="menu__columns">
+                <div class="menu__row menu__row--th menu__row--col3">
+                  <div>
+                  </div>
+                  <div class="text--center">
+                    {!! $content['size1'] !!}
+                  </div>
+                  <div class="text--center">
+                    {!! $content['size2'] !!}
+                  </div>
+                </div>
+                <div class="menu__row menu__row--th menu__row--col3">
+                  <div>
+                  </div>
+                  <div class="text--center">
+                    {!! $content['size1'] !!}
+                  </div>
+                  <div class="text--center">
+                    {!! $content['size2'] !!}
+                  </div>
+                </div>
+              </div>
+            @endif
+            <div class="menu__columns" style="grid-auto-flow: column; grid-template-rows: repeat({{ $countrows }}, 1fr);">
+              @foreach($content['list'] as $item)
+                <div class="menu__row @if($content['menu-type'] == 'pizza') menu__row--col3 @endif">
+                    <div>
                       <span class="text--bold">
                         {{ $loop->index+1 }}. {{ $item['name'] }}
                       </span>
-                      <br>
-                      ({{ $item['desc'] }})
-                    </td>
-                    <td>{{ $item['price'] }} zł</td>
-                    <td>{{ $item['price2'] }} zł</td>
-                  </tr>
-                @endif
+                      @if($item['desc'])
+                        <br>
+                        ({{ $item['desc'] }})
+                      @endif
+                    </div>
+                    <div class="text--center">
+                      {{ $item['price'] }} zł
+                    </div>
+                    @if($content['menu-type'] == 'pizza')
+                      <div class="text--center">
+                        @if($item['price2'])
+                          {{ $item['price2'] }} zł
+                        @endif
+                      </div>
+                    @endif
+                </div>
               @endforeach
-            </table>
-            <table class="table">
-              @foreach(get_option_field("menu")[0]['content']['list'] as $item)
-                @php
-                  $half = $loop->count/2
-                @endphp
-                @if($loop->iteration > $half)
-                  <tr>
-                    <td>
-                      <span class="text--bold">
-                        {{ $loop->index+1 }}. {{ $item['name'] }}
-                      </span>
-                      <br>
-                      ({{ $item['desc'] }})
-                    </td>
-                    <td>{{ $item['price'] }} zł</td>
-                    <td>{{ $item['price2'] }} zł</td>
-                  </tr>
-                @endif
-              @endforeach
-            </table>
+            </div>
           </div>
-        </div>
+        @endforeach
       </div>
     </div>
 </section>
