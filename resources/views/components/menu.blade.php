ff805ef40262cb546568bba6ab72f832570b57d6
rafaliq <rafal@creativeroom.pl>
2020-04-23 09:15:25 +0200

    menu two table and desc

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -55,12 +55,12 @@
                 </div>
               </div>
             @endif
-            <div class="menu__columns @if($content['columns'] == '1') menu__columns--one @endif" style="grid-auto-flow: column; grid-template-rows: repeat({{ $countrows }}, 1fr);">
+            <div class="menu__columns @if($content['columns'] == '1') menu__columns--one @endif" style="grid-auto-flow: column; grid-template-rows: repeat({{ round($countrows) }}, 1fr);">
               @foreach($content['list'] as $item)
                 <div class="menu__row @if($content['columns'] == '1') menu__row--space @endif @if($content['menu-type'] == 'pizza') menu__row--col3 @endif">
                     <div>
                       <span class="text--bold">
-                        @if($content['menu-type'] == 'pizza')
+                        @if(($content['menu-type'] == 'pizza') && (strpos($item['name'], 'Dodatki') === false))
                           {{ $loop->index+1 }}.
                         @endif
                         {{ $item['name'] }}
@@ -83,6 +83,32 @@
                 </div>
               @endforeach
             </div>
+            @if($content['twolist'] == 'tak')
+              <div class="menu__table">
+                <span class="text text--bold text--primary menu__title">
+                  {{ $content['list2title'] }}
+                </span>
+                <div class="menu__columns menu__columns--one">
+                  @foreach($content['list2'] as $item)
+                    <div class="menu__row menu__row--space">
+                      <div>
+                        <span class="text--bold">
+                          {{ $item['name'] }}
+                        </span>
+                      </div>
+                      <div class="text--center">
+                        {{ $item['price'] }} zł
+                      </div>
+                    </div>
+                  @endforeach
+                </div>
+              </div>
+            @endif
+            @if($content['desc'])
+              <div class="text menu__text">
+                {!! $content['desc'] !!}
+              </div>
+            @endif
           </div>
         @endforeach
       </div>
